2701c1c6e55bba5f4ddbb2c4e23160e0ead2aab2
Ayesh chamodye <109528815+ayeshchamodye@users.noreply.github.com>
2024-02-10 09:04:23 +0530

    Update session.sh

--- session.sh
+++ session.sh
@@ -1,7 +1,8 @@
 #!bin/bash
 #!bin/fish
 
-if ![test -f session.rc]; then
+if ! [ -f session.rc ]; then
         wget https://raw.githubusercontent.com/ayeshchamodye/Metasploit/main/session.rc
 fi
+
 msfconsole -r session.rc
